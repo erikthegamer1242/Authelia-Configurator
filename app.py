61e5b0961336ae680efb88ed248e2d7cf12e6055
erikthegamer1242 <erikbraun1242@gmail.com>
2023-07-17 18:59:55 +0200

    The main list is somewhat done, except css, and deleating, now getting started on the edit interface

--- app.py
+++ app.py
@@ -14,7 +14,14 @@ def read_one_block_of_yaml_data(filename):
 @app.route('/rules', methods=['GET', 'POST'])
 def index():
     acl = (read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"])
+    for idx, rule in enumerate(acl):
+        print(idx, rule)
     return render_template('rules.html', acl=acl)
+
+
+@app.route('/edit_rule/<int:id>', methods=['GET', 'POST'])
+def edit_rule(id):
+    return read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"][id], 200
 
 
 @app.route('/other_config', methods=['GET', 'POST'])
