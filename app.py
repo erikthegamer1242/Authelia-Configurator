4784f0e37a106e8bb225c48032d5ef918c6a8618
erikthegamer1242 <erikbraun1242@gmail.com>
2023-07-17 16:40:51 +0200

    Testing commit signing

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'THis commit is just to test if my commits are being properly signed'
 
 
 if __name__ == '__main__':
