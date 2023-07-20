481c73a9b2c5b5bf69353aab35b1d86e0afb3fe6
erikthegamer1242 <erikbraun1242@gmail.com>
2023-07-20 23:03:58 +0200

    The edit_rule UI is comming along good, added deleting, returning nice POST dat, and also saving changes, only need to add JS to add more domains, and other options like resources, policy... Then we finish eveything else

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
 from flask import Flask, render_template, request, redirect
-import yaml
-from yaml import SafeLoader, FullLoader
+import ruamel.yaml
+import ast
+
+yaml = ruamel.yaml.YAML()
 
 app = Flask(__name__)
 
 
 def read_one_block_of_yaml_data(filename):
     with open(f'{filename}', 'r') as f:
-        output = list(yaml.safe_load_all(f))
+        output = list(yaml.load_all(f))
     return output
 
 
-@app.route('/rules', methods=['GET', 'POST'])
+def save_yaml(data, filename):
+    with open(filename, 'w') as stream:
+        yaml.dump_all(data, stream)
+
+
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/rules', methods=['GET', 'POST'])
+def rules():
     acl = (read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"])
-    for idx, rule in enumerate(acl):
-        print(idx, rule)
+    # for idx, rule in enumerate(acl):
+    # print(idx, rule)
+    # print(read_one_block_of_yaml_data('configuration.yml'))
     return render_template('rules.html', acl=acl)
 
 
 @app.route('/edit_rule/<int:id>', methods=['GET', 'POST'])
 def edit_rule(id):
-    return read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"][id], 200
+    # return read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"][id], 200
+    if request.method == 'POST':
+        domains = ast.literal_eval(request.form['domainsArr'])
+        policy = request.form['policy']
+        config = read_one_block_of_yaml_data('configuration.yml')
+        config[0]["access_control"]["rules"][id]["domain"] = domains
+        config[0]["access_control"]["rules"][id]["policy"] = policy
+        save_yaml(config, 'configuration.yml')
+        return redirect('/rules')
+    return render_template('edit_rule.html',
+                           rule=read_one_block_of_yaml_data('configuration.yml')[0]["access_control"]["rules"][id],
+                           id=id)
 
 
 @app.route('/other_config', methods=['GET', 'POST'])
@@ -30,4 +54,4 @@ def other_config():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
